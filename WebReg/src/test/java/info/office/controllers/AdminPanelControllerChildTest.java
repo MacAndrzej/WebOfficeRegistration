2753015e5edad37f1bc6642da986f491d5f25936
MacAndrzej <maciejukandrzej@gmail.com>
2018-07-08 21:33:18 +0200

    1.Pozostałe testy do kontrolerów

--- WebReg/src/test/java/info/office/controllers/AdminPanelControllerChildTest.java
+++ WebReg/src/test/java/info/office/controllers/AdminPanelControllerChildTest.java
@@ -1,28 +1,46 @@
 package info.office.controllers;
 
+import static org.hamcrest.CoreMatchers.any;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.BindingResult;
 
 import info.office.dto.ChildEntityBuilderImpl;
+import info.office.dto.ParentEntityBuilderImpl;
+import info.office.dto.VisitEntityBuilderImpl;
 import info.office.entity.Child;
+import info.office.entity.Parent;
+import info.office.entity.Visit;
 import info.office.service.ChildService;
 
 public class AdminPanelControllerChildTest {
@@ -30,69 +48,107 @@ public class AdminPanelControllerChildTest {
 	private MockMvc mockMvc;
 
 	@Mock
-	private ChildService childService;
+	ChildService childService;
 
 	@InjectMocks
-	private AdminPanelControllerChild adminPanelControllerChild;
+	AdminPanelControllerChild controller;
 
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
-		mockMvc = MockMvcBuilders.standaloneSetup(adminPanelControllerChild).build();
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 	}
 
 	@Test
 	public void testListChildren_ListIsNotEmpty() throws Exception {
-		// given
-		List<Child> children = new ArrayList<>();
-		children.add(new Child());
-		children.add(new Child());
+		// for
+		Child first = new ChildEntityBuilderImpl().id(1L).build();
+		Child second = new ChildEntityBuilderImpl().id(2L).build();
+		List<Child> children = Arrays.asList(first, second);
 
 		// when
 		when(childService.getChildren()).thenReturn(children);
 
 		// then
-		mockMvc.perform(get("/admin/listChildren")).andExpect(view().name("list-children")).andExpect(status().isOk());
-		
-		verify(childService, times(1)).getChildren();
+		mockMvc.perform(get("/admin/listChildren")).andExpect(view().name("list-children")).andExpect(status().isOk())
+				.andExpect(model().attributeExists("children")).andExpect(model().size(1))
+				.andExpect(model().attribute("children", children));
 	}
 
 	@Test
 	public void testListChildren_ListIsEmpty() throws Exception {
-		// given
+		// for
 		List<Child> children = new ArrayList<>();
 
 		// when
 		when(childService.getChildren()).thenReturn(children);
 
 		// then
-		mockMvc.perform(get("/admin/listChildren")).andExpect(model().size(1)).andExpect(view().name("list-children"))
-				.andExpect(status().isOk());
-		
-		verify(childService, times(1)).getChildren();
+		mockMvc.perform(get("/admin/listChildren")).andExpect(view().name("list-children")).andExpect(status().isOk())
+				.andExpect(model().attributeExists("children")).andExpect(model().size(1))
+				.andExpect(model().attribute("children", children));
 	}
 
 	@Test
 	public void testShowFormForAddChild() throws Exception {
-		
+
 		mockMvc.perform(get("/admin/showFormForAddChild")).andExpect(view().name("child-form"))
 				.andExpect(status().isOk()).andExpect(model().attribute("children", instanceOf(Child.class)))
-				.andDo(print());
+				.andExpect(view().name("child-form"));
 	}
-	
+
 	@Test
-	public void testShowFormForUpdateChild_EntryExists() throws Exception {
-//		given
-		Child first=new ChildEntityBuilderImpl().id(1L).build();
-		
-//		when
-		when(childService.findByParent_id(1L)).thenReturn(first);
-		
-//		then
-		mockMvc.perform(get("/admin/listChildren?childrenId=1")).andExpect(view().name("list-children"))
-				.andExpect(status().isOk()).andExpect(model().attributeExists("children"))
-				.andDo(print());
+	public void testSaveChild_NoErrorsAfterValidation() throws Exception {
+		// for
+		Child first = new ChildEntityBuilderImpl().id(2L).dateOfBirth(LocalDate.of(1974, 8, 26)).name("andrzej")
+				.surname("andrzej").visit(null).parent(null).build();
+
+		// when
+		when(childService.saveChild(first)).thenReturn(first);
+
+		childService.saveChild(first);
+
+		// then
+		mockMvc.perform(
+				post("/admin/saveChild").param("id", "2").param("dateOfBirth", LocalDate.of(1974, 8, 26).toString())
+						.param("name", "andrzej").param("surname", "andrzej"))
+				.andExpect(view().name("redirect:/admin/listChildren")).andExpect(status().is3xxRedirection());
+
+		verify(childService, times(1)).saveChild(first);
 	}
-	
+
+	@Test
+	public void testSaveChild_ErrorsAfterValidation() throws Exception {
+		// for
+		Child first = new ChildEntityBuilderImpl().id(2L).build();
+
+		// when
+		when(childService.saveChild(first)).thenReturn(first);
+
+		// then
+		mockMvc.perform(post("/admin/saveChild").param("id", "2")).andExpect(view().name("child-form"));
+	}
+
+	@Test
+	public void testUpdateChild() throws Exception {
+		// for
+		Child first = new ChildEntityBuilderImpl().id(1L).build();
+
+		// when
+		when(childService.getChild(anyLong())).thenReturn(first);
+
+		// then
+		mockMvc.perform(get("/admin/showFormForUpdateChild?childId=1")).andExpect(status().isOk())
+				.andExpect(view().name("child-form"));
+	}
+
+	@Test
+	public void testDeleteChild() throws Exception {
+
+		mockMvc.perform(get("/admin/deleteChild?childId=1")).andExpect(status().is3xxRedirection())
+				.andExpect(view().name("redirect:/admin/listChildren"));
+
+		verify(childService, times(1)).deleteChild(anyLong());
+	}
 
 }
--- WebReg/src/test/java/info/office/controllers/WebControllerTest.java
+++ WebReg/src/test/java/info/office/controllers/WebControllerTest.java
@@ -3,7 +3,10 @@ package info.office.controllers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -11,32 +14,37 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 public class WebControllerTest {
 
 	private MockMvc mockMvc;
+	
+	@InjectMocks
+	WebController controller;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
 
 	@Test
 	public void testAbout() throws Exception {
-		WebController controller = new WebController();
-		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
 		mockMvc.perform(get("/about")).andExpect(MockMvcResultMatchers.view().name("aboutPage"));
 	}
 
 	@Test
 	public void testCertificates() throws Exception {
-		WebController controller = new WebController();
-		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
 		mockMvc.perform(get("/certificates")).andExpect(view().name("certificatesPage"));
 	}
 
 	@Test
 	public void testContact() throws Exception {
-		WebController controller = new WebController();
-		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
 		mockMvc.perform(get("/contact")).andExpect(view().name("contactPage"));
 	}
 
 	@Test
 	public void testShowMainPage() throws Exception {
-		WebController controller = new WebController();
-		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
 		mockMvc.perform(get("/home", "/")).andExpect(view().name("main-menu"));
 	}
 
